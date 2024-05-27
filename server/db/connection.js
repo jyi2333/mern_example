10ab9fe1cf8b974630870069278e07e023790e68
Jingwen Yi <jyi@smail.uni-koeln.de>
2024-05-27 15:41:31 +0200

    reconstruction

---
+++ server/db/connection.js
@@ -0,0 +1,28 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+
+//use node >=20 if error code on --env-file
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+try {
+  // Connect the client to the server
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log(
+   "Pinged your deployment. You successfully connected to MongoDB!"
+  );
+} catch(err) {
+  console.error(err);
+}
+
+let db = client.db("employees");
+
+export default db;
